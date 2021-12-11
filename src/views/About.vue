04b2fe57b93a43bff20db838e94bf3f15a050736
rajendraprasadp <rajuprasadp1@gmail.com>
2021-12-11 18:42:26 +0530

    changes!

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,10 +1,10 @@
 <template>
-  <router-link to="/"> go back</router-link>
+  <router-link to="/"> <h4>go back</h4></router-link>
   <h2>Thanks for visiting!</h2>
   <form>
     <div class="container">
-      <label>Suggestions</label>
-      <input type="text" placeholder="any suggestions" />
+      <h1>CONTACT</h1>
+      <label>rajuprasadp1@gmail.com</label>
     </div>
   </form>
 </template>
@@ -24,7 +24,7 @@ h2 {
 
 label {
   display: inline-block;
-  background: rgb(180, 34, 34);
+  background: rgb(49, 190, 68);
   color: #fff;
   border: none;
   padding: 10px 20px;
@@ -40,10 +40,25 @@ label {
 .container {
   max-width: 500px;
   margin: 30px auto;
+  text-align: center;
   overflow: auto;
   min-height: 300px;
   border: 1px solid steelblue;
   padding: 30px;
   border-radius: 5px;
+}
+router-link {
+  display: inline-block;
+  background: rgb(180, 34, 34);
+  color: #fff;
+  border: none;
+  padding: 10px 20px;
+  margin: 5px;
+  border-radius: 5px;
+  cursor: pointer;
+  text-decoration: none;
+  font-size: 15px;
+  font-family: inherit;
+  text-align: center;
 }
 </style>
